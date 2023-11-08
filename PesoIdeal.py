034a20ca3d8ea527c74584a6ff6e8f19e5f812a2
Rodrigo Camilo <60991851+rodcamilo@users.noreply.github.com>
2023-11-08 14:29:41 -0300

    Add files via upload

--- PesoIdeal.py
+++ PesoIdeal.py
@@ -14,17 +14,22 @@ while True:
     print("Seu IMC é",round(imc,2))
     if imc < 17:
         print("Você está MUITO ABAIXO do peso/massa ideal.\nSeu peso/massa ideal é entre",round(alvomin,2),"e",round(alvomax,2),"quilogramas.")
+        print("####\n")
     elif imc < 18.5:
         print("Você está ABAIXO do peso/massa ideal.\nSeu peso/massa ideal é entre",round(alvomin,2),"e",round(alvomax,2),"quilogramas.")
+        print("####\n")
     elif imc < 25:
         print("Você está com o peso/massa IDEAL.\nSeu peso/massa ideal é entre",round(alvomin,2),"e",round(alvomax,2),"quilogramas.")
+        print("####\n")
     elif imc < 30:
         print("Você está ACIMA do peso/massa ideal.\nSeu peso/massa ideal é entre",round(alvomin,2),"e",round(alvomax,2),"quilogramas.")
+        print("####\n")
     elif imc < 35:
         print("Você está OBESO.\nSeu peso/massa ideal é entre",round(alvomin,2),"e",round(alvomax,2),"quilogramas.")
+        print("####\n")
     elif imc < 40:
         print("Você está em OBESIDADE SEVERA.\nSeu peso/massa ideal é entre",round(alvomin,2),"e",round(alvomax,2),"quilogramas.")
+        print("####\n")
     else:
         print("Você está em OBESIDADE MÓRBIDA.\nSeu peso/massa ideal é entre",round(alvomin,2),"e",round(alvomax,2),"quilogramas.")
-    break
-print("####")
+        print("####\n")
